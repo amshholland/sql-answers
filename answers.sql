1d181e8df76d5ac7ac73954618c5e912ee6ce895
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-26 09:05:21 -0400

    numbered answers

--- answers.sql
+++ answers.sql
@@ -11,46 +11,46 @@ SELECT * FROM customers
 -- 4. Get the first 100 records of the orders table.
 SELECT * FROM customers LIMIT 100;
 
--- Get all customers that live in the postal codes 1010, 3012, 12209, and 05023.
+-- 5. Get all customers that live in the postal codes 1010, 3012, 12209, and 05023.
 SELECT * FROM customers 
     WHERE postal_code IN ('1010', '3012', '12209', '05023');
 
--- Get all orders where the ShipRegion is not equal to NULL.
+-- 6. Get all orders where the ShipRegion is not equal to NULL.
 SELECT * FROM orders 
     WHERE ship_region IS NOT null;
 
--- Get all customers ordered by the country, then by the city.
+-- 7. Get all customers ordered by the country, then by the city.
 SELECT * FROM orders 
     ORDER BY ship_country, ship_city;
 
--- Add a new customer to the customers table. You can use whatever values/
+-- 8. Add a new customer to the customers table. You can use whatever values/
 INSERT INTO customers (customer_id, company_name, contact_name, contact_title, 
     address, city, region, postal_code, country, phone, fax)
     VALUES (69, 'Dunder Mifflin', 'Kevin Malone', 'Accountant', '1725 Slough Avenue', 'Scranton', 
 	'Middle Atlantic', 18503, 'United States', 1-800-984-3672, 1-800-984-3672  );
 
--- Update all ShipRegion to the value ‘EuroZone’ in the Orders table, where the ShipCountry is equal to France.  
+-- 9. Update all ShipRegion to the value ‘EuroZone’ in the Orders table, where the ShipCountry is equal to France.  
 UPDATE orders
 	SET ship_region='EuroZone'
 	WHERE ship_country='France';
 
--- Delete all orders from `order_details` that have a quantity of 1. 
+-- 10. Delete all orders from `order_details` that have a quantity of 1. 
 DELETE FROM order_details
 	WHERE quantity=1;
 
--- Calculate the average, max, and min of the quantity at the `order details` table.
+-- 11. Calculate the average, max, and min of the quantity at the `order details` table.
 SELECT AVG(quantity) AS average, MAX(quantity) AS max, MIN(quantity) AS min 
     FROM order_details;
 
--- Calculate the average, max, and min of the quantity at the `order details` table, grouped by the orderid. 
+-- 12. Calculate the average, max, and min of the quantity at the `order details` table, grouped by the orderid. 
 SELECT AVG(quantity) AS average, MAX(quantity) AS max, MIN(quantity) AS min 
     FROM order_details 
     GROUP BY order_id;
 
--- Find the CustomerID that placed order 10290 (orders table)
+-- 13. Find the CustomerID that placed order 10290 (orders table)
 SELECT customer_id FROM orders WHERE order_id = 10290;
 
--- Do an inner join, left join, right join on orders and customers tables.  (These are three separate queries, one for each type of join.)
+-- 14. Do an inner join, left join, right join on orders and customers tables.  (These are three separate queries, one for each type of join.)
 SELECT orders.customer_id, customers.contact_name,  orders.order_id FROM orders
 JOIN customers ON  orders.customer_id = customers.customer_id;
 
@@ -62,23 +62,23 @@ SELECT employee_id, contact_title, contact_name FROM orders
 LEFT JOIN customers 
 ON orders.customer_id = customers.customer_id;
 
--- Use a join to get the ship city and ship country of all the 
+-- 15. Use a join to get the ship city and ship country of all the 
 -- orders which are associated with an employee who is in London.
 SELECT orders.ship_city, orders.ship_country FROM orders
 LEFT JOIN employees
 ON employees.city = 'London';
 
--- Use a join to get the ship name of all orders that include a 
+-- 16. Use a join to get the ship name of all orders that include a 
 -- discontinued product. (See orders, order_details, and products. 
 -- 1 means discontinued.)
 SELECT orders.ship_name FROM orders
 JOIN products
 ON products.discontinued = 1;
 
--- Get first names of all employees who report to no one.
+-- 17. Get first names of all employees who report to no one.
 SELECT first_name FROM employees WHERE reports_to IS null;
 
--- Get first names of all employees who report to Andrew.
+-- 18. Get first names of all employees who report to Andrew.
 SELECT * FROM employees AS employee_first_name
 JOIN employee_id employee_id
 ON employee_first_name.first_name = 'Andrew';
